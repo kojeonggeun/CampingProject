2ce3b76535ef735ce68b0e87967fe971ce13df1f
kojunggun <ggggg46@naver.com>
2021-08-12 22:01:52 +0900

    장비 관련 파일 추가

---
+++ CampingProject/Gear/AddGearViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddGear.swift
+//  CampingProject
+//
+//  Created by 고정근 on 2021/08/12.
+//
+
+import Foundation
+import UIKit
+
+
+class AddGearViewController: UIViewController{
+    
+    @IBAction func closeButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+}
